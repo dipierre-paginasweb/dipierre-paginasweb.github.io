4a2f481cf9431d17c40fb9c48a1351fee7bc820f
taamiserrano <147259939+taamiserrano@users.noreply.github.com>
2024-02-21 17:35:09 -0300

    w1

---
+++ Front-end/Wedding/W1/js/GoogleSheet.js
@@ -0,0 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyIUVks8Teo1Tf2-eF0ObSXuvPRrIfCvoqVO2puV5reHUZlT4FSWIom8cWwoRxJmEUs/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => {
+    if (response.ok) {
+      window.location.href = 'https://dipierre-paginasweb.github.io/Back-end/Wedding/W1/Succesfull.html'; // Cambia 'tu_pagina_de_destino.html' por la URL de la página a la que deseas redirigir.
+    } else {
+      alert("Hubo un error al enviar el formulario.");
+    }
+  })
+  .catch(error => console.error('¡Error!', error.message))
+ })

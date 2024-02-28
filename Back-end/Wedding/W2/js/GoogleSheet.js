36b701054ef3f63ea2169bf59fc0672076bf94c8
taamiserrano <147259939+taamiserrano@users.noreply.github.com>
2024-02-28 20:41:47 -0300

    W2

--- Back-end/Wedding/W2/js/GoogleSheet.js
+++ Back-end/Wedding/W2/js/GoogleSheet.js
@@ -1,11 +1,16 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx95FEYh4ddbxd58HvBWPtV5Z-p7zon-yAMK3BlkrYa3mQUyIAFW14qoAz7Sy_mibIL/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyIUVks8Teo1Tf2-eF0ObSXuvPRrIfCvoqVO2puV5reHUZlT4FSWIom8cWwoRxJmEUs/exec'
 
 const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Muchas gracias por tu respuesta!" ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+  .then(response => {
+    if (response.ok) {
+      window.location.href = 'https://dipierre-paginasweb.github.io/Back-end/Wedding/W1/Succesfull#success.html'; // Cambia 'tu_pagina_de_destino.html' por la URL de la página a la que deseas redirigir.
+    } else {
+      alert("Hubo un error al enviar el formulario.");
+    }
+  })
+  .catch(error => console.error('¡Error!', error.message))
+ })
